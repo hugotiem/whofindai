03a0256d1258e79a3ec08d7b8edbae1299f7c4bf
hugotiem <hugotiem@gmail.com>
2025-03-10 18:49:57 +0100

    fux

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -1,4 +1,4 @@
-export const maxDuration = 120; // This function can run for a maximum of 60 seconds
+export const maxDuration = 60; // This function can run for a maximum of 60 seconds
 
 import { NextRequest, NextResponse } from 'next/server';
 import { promptContext, PromptProps } from './prompt';
