05d2880f753d55e543aa50b513f940b99dc372b1
hugotiem <hugotiem@gmail.com>
2025-06-09 18:06:59 +0200

    added linkedin tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 import { Toaster } from '@/components/ui/sonner';
 import { ThemeProvider } from '@/components/custom/theme-provider';
 import { AnalyticsProvider } from '@/providers/analyticsProvider';
+import Script from 'next/script';
 // import { Squares } from '@/components/ui/squares-background';
 // import { memo } from 'react';
 
@@ -70,6 +71,40 @@ export default async function RootLayout({
             <Toaster />
           </ThemeProvider>
         </AnalyticsProvider>
+        <footer>
+          <Script id="linkedin-partner-setup" strategy="afterInteractive">
+            {`
+            _linkedin_partner_id = "7186458";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `}
+          </Script>
+          <Script id="linkedin-tracking" strategy="afterInteractive">
+            {`
+            (function(l) {
+              if (!l){
+                window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                window.lintrk.q=[]
+              }
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";
+              b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `}
+          </Script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              alt=""
+              src="https://px.ads.linkedin.com/collect/?pid=7186458&fmt=gif"
+            />
+          </noscript>
+        </footer>
       </body>
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_SA_MEASUREMENT_ID!} />
     </html>
